1dca59e2274a840d44b760595785d1927a87d566
Hansanguk <hansanguk0222@gmail.com>
2021-10-05 20:59:17 +0900

    [feat] 기본 셋업

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  env: {
+    node: true,
+  },
+  extends: ['plugin:@typescript-eslint/recommended'],
+};
